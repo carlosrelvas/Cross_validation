8e5fbba421aefefc71ddb70bffa17d872d892340
carlosrelvas <carlos.edu.relvas@gmail.com>
2015-03-16 00:20:22 -0300

    v2
    v2

--- cross_validation.R
+++ cross_validation.R
@@ -25,36 +25,44 @@ library(caret)
 fitControl <- trainControl(## 10-fold CV
   method = "cv", number = 10, classProbs = TRUE, summaryFunction = twoClassSummary)
 
+ctreeGrid <-  expand.grid(.maxdepth=c(10,15,20))
+
 set.seed(4322)
 ptm <- proc.time()
 fit <- train(as.formula(formula), data = data.train,
-                 method = "glm",
+                 method = "ctree2",
                  trControl = fitControl,
-                 metric = "ROC")
+                 metric = "ROC",
+                 tuneGrid = ctreeGrid)
 proc.time() - ptm
 
 #usuário   sistema decorrido 
-#18.64      0.02     18.69
+#81.47      0.04     82.42 
 
 fit
 
-#Generalized Linear Model #
-#
+#Conditional Inference Tree 
+
 #3000 samples
 #57 predictor
-#2 classes: 'NotSpam', 'Spam' #
-#
+#2 classes: 'NotSpam', 'Spam' 
+
 #No pre-processing
-#Resampling: Cross-Validated (10 fold) #
-#
-#Summary of sample sizes: 2700, 2700, 2700, 2700, 2700, 2700, ... #
-#
-#Resampling results
-#
-#ROC        Sens       Spec       ROC SD       Sens SD     Spec SD   
-#0.9686324  0.9428571  0.8788136  0.008807609  0.02216042  0.02826271
+#Resampling: Cross-Validated (10 fold) 
+
+#Summary of sample sizes: 2700, 2700, 2699, 2700, 2701, 2700, ... 
+
+#Resampling results across tuning parameters:
+  
+#  maxdepth  ROC        Sens       Spec       ROC SD      Sens SD     Spec SD   
+#10        0.9462662  0.9272356  0.8636732  0.01309512  0.01990168  0.03562211
+#15        0.9508955  0.9245241  0.8662669  0.01222702  0.02296164  0.03661791
+#20        0.9495368  0.9234371  0.8671289  0.01239722  0.02435236  0.03660957
+
+#ROC was used to select the optimal model using  the largest value.
+#The final value used for the model was maxdepth = 15. 
 
 diag(data.train[,"V58"], predict(fit, newdata=data.train, type="prob")[,2])
-#[1] 0.9779449
+#[1] 0.9553383
 diag(data.test[,"V58"], predict(fit, newdata=data.test, type="prob")[,2])
-#[1] 0.97379
+#[1] 0.9292543
